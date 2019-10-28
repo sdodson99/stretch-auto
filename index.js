2ce4052990f4f781698a3ebece9b79f0c621ff5d
Sean Dodson <dodsonsean@comcast.net>
2019-10-28 15:10:11 -0400

    Add connection string environment variable

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
-const connectionString = "mongodb://stretch-db:27017/"
+const connectionString = process.env.STRETCH_CONNECTION_STRING || "mongodb://localhost:27017/"
 
 app.use("/stretch", new StretchRouter(connectionString).getRouter())
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}.`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`))
