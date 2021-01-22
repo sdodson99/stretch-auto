21ae2d82b796926a8fc8f785058ca482ac88cfe7
Sean Dodson <dodsonsean@comcast.net>
2021-01-21 21:35:51 -0500

    Add loading

--- client/src/app/live-routine/live-routine.component.ts
+++ client/src/app/live-routine/live-routine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Routine from '../models/routine';
+import { RoutineService } from '../services/routine.service';
 
 @Component({
   selector: 'app-live-routine',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveRoutineComponent implements OnInit {
 
-  constructor() { }
+  hasNoStretches = false;
+  isLoadingRoutine = true;
+
+  constructor(private routineService: RoutineService) { }
 
   ngOnInit(): void {
+    this.routineService.getRountine().subscribe(routine => this.startRoutine(routine));
   }
 
+  startRoutine(routine: Routine): void {
+    this.isLoadingRoutine = false;
+
+    if (routine.stretches.length === 0) {
+      this.hasNoStretches = true;
+      return;
+    }
+  }
 }
